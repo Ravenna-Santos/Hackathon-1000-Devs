f8898ed6f6dffda471b19461d23710728ff26405
Luan Sena <86327154+LUANwesDEV@users.noreply.github.com>
2023-12-10 15:52:12 -0300

    Adiciona cadastro campanha

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const rotaPaciente = require('./rotas/paciente.js');
 //const rotaVacina = require('./rotas/vacina.js');
+const rotaCampanha = require('./rotas/campanha.js');
+
 
 const app = express();
 
@@ -8,6 +10,7 @@ app.use(express.json());
 
 app.use('/paciente', rotaPaciente);
 //app.use('/vacina', rotaVacina);
+app.use('/campanha', rotaCampanha);
 
 const port = 3000;
 
