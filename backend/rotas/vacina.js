2e3885708093030d70b47372b598e39da2f2c45e
Ravenna-Santos <ravennasantos2016@gmail.com>
2023-12-10 10:40:25 -0300

    consulta vacina por idade

--- backend/rotas/vacina.js
+++ backend/rotas/vacina.js
@@ -127,4 +127,130 @@ rotaVacina.get('/pedente/paciente/:id', async (req, res) => {
 
 })
 
+/*
+API de pesquisa de vacina por ano (exato)
+*/
+rotaVacina.get('/ano-exato/:ano', async (req, res) => {
+    //obtem o parametro
+    const ano = req.params.ano;
+
+    console.log(`=> endpoint /vacina/ano-exato/${ano} requisitado`);
+
+    try {
+        const vacina = await pool.query(`select v.id_vacina, v.vacina,
+        v.sigla_vacina, pa.qtd_ano_inicial, pa.qtd_ano_final  from vacina v
+        inner join  PERIODOAPLICACAOANO PA on v.id_vacina = pa.id_vacina
+        where pa.qtd_ano_inicial = ${ano};`);
+
+        //verifica se retornou alguma linha do banco de dados
+        if (vacina.rowCount > 0){
+            //se retornou devolve os dados com o status 200
+            res.status(200).send(vacina.rows);
+        } else {
+            //caso nao retorne nenhuma linha o id nao existe
+            res.status(404).json({mensagem: 'Vacina nao encontrado.'})
+        }
+    } catch (error) {    
+        console.log(error);
+        res.status(404).json({mensagem: 'Erro ao executar a consulta.'});
+    }
+
+})
+
+/*API de pesquisa de vacina por ano (até)*/
+rotaVacina.get('/ano-ate/:ano', async (req, res) => {
+    //obtem o parametro
+    const ano = req.params.ano;
+
+    console.log(`=> endpoint /vacina/ano-ate/${ano} requisitado`);
+
+    try {
+        const vacina = await pool.query(`select v.id_vacina, v.vacina,
+        v.sigla_vacina, pa.qtd_ano_inicial, pa.qtd_ano_final ,
+        pm.qtd_meses_inicial
+        from vacina v
+        left join  PERIODOAPLICACAOANO PA on v.id_vacina = pa.id_vacina
+        left join  PERIODOAPLICACAOMES PM on v.id_vacina = pm.id_vacina
+        where pa.qtd_ano_inicial <= ${ano}
+       or (pm.qtd_meses_inicial / 12) <= ${ano};`);
+
+        //verifica se retornou alguma linha do banco de dados
+        if (vacina.rowCount > 0){
+            //se retornou devolve os dados com o status 200
+            res.status(200).send(vacina.rows);
+        } else {
+            //caso nao retorne nenhuma linha o id nao existe
+            res.status(404).json({mensagem: 'Vacina nao encontrado.'})
+        }
+    } catch (error) {    
+        console.log(error);
+        res.status(404).json({mensagem: 'Erro ao executar a consulta.'});
+    }
+
+})
+
+/*
+API de pesquisa de vacina por mes (exato)
+*/
+rotaVacina.get('/mes-exato/:mes', async (req, res) => {
+    //obtem o parametro
+    const mes = req.params.mes;
+
+    console.log(`=> endpoint /vacina/mes-exato/${mes} requisitado`);
+
+    try {
+        const vacina = await pool.query(`select v.id_vacina, v.vacina,
+        v.sigla_vacina, pm.qtd_meses_inicial , pm.qtd_meses_final from vacina v
+        inner join  PERIODOAPLICACAOMES PM on v.id_vacina = pm.id_vacina
+        where pm.qtd_meses_inicial = ${mes};`);
+
+        //verifica se retornou alguma linha do banco de dados
+        if (vacina.rowCount > 0){
+            //se retornou devolve os dados com o status 200
+            res.status(200).send(vacina.rows);
+        } else {
+            //caso nao retorne nenhuma linha o id nao existe
+            res.status(404).json({mensagem: 'Vacina nao encontrado.'})
+        }
+    } catch (error) {    
+        console.log(error);
+        res.status(404).json({mensagem: 'Erro ao executar a consulta.'});
+    }
+
+})
+
+/*API de pesquisa de vacina por mes (até)*/
+rotaVacina.get('/mes-ate/:mes', async (req, res) => {
+    //obtem o parametro
+    const mes = req.params.mes;
+
+    console.log(`=> endpoint /vacina/ano-mes/${mes} requisitado`);
+
+    try {
+        const vacina = await pool.query(`select v.id_vacina, v.vacina,
+        v.sigla_vacina, pm.qtd_meses_inicial , pm.qtd_meses_final from vacina v
+        inner join  PERIODOAPLICACAOMES PM on v.id_vacina = pm.id_vacina
+        where pm.qtd_meses_inicial <= ${mes};`);
+
+        //verifica se retornou alguma linha do banco de dados
+        if (vacina.rowCount > 0){
+            //se retornou devolve os dados com o status 200
+            res.status(200).send(vacina.rows);
+        } else {
+            //caso nao retorne nenhuma linha o id nao existe
+            res.status(404).json({mensagem: 'Vacina nao encontrado.'})
+        }
+    } catch (error) {    
+        console.log(error);
+        res.status(404).json({mensagem: 'Erro ao executar a consulta.'});
+    }
+
+})
+
+
+
+
+
+
+
 module.exports = rotaVacina; 
